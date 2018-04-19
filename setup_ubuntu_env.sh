65068def7d3459bb9654118ac7671259533a2b25
unknown <263833322@qq.com>
2018-04-19 11:30:50 +0800

    add bashrc and vim install

--- setup_ubuntu_env.sh
+++ setup_ubuntu_env.sh
@@ -3,7 +3,9 @@
 #sudo apt-get -f install
 #sudo apt-get update
 #sudo dpkg -i bcompare-3.3.13.18981_i386.deb
+# ~/.bashrc  1.color_prompt=yes  2.\w -> \W
 
+#android tools
 sudo apt-get install uuid uuid-dev
 sudo apt-get install zlib1g-dev liblz-dev
 sudo apt-get install liblzo2-2 liblzo2-dev
@@ -15,6 +17,9 @@ sudo apt-get install android-tools-fsutils
 sudo apt-get install openjdk-8-jdk
 sudo apt-get install device-tree-compiler
 sudo apt-get install gdisk
+
+#other tools
+sudo apt-get install vim
 
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
