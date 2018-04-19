5320ad5c5f0a2b45f6b35e5c6b9906ba992a15f7
unknown <263833322@qq.com>
2018-04-19 11:24:14 +0800

    add ubuntu env shell

---
+++ setup_ubuntu_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#sudo apt-get -f install
+#sudo apt-get update
+#sudo dpkg -i bcompare-3.3.13.18981_i386.deb
+
+sudo apt-get install uuid uuid-dev
+sudo apt-get install zlib1g-dev liblz-dev
+sudo apt-get install liblzo2-2 liblzo2-dev
+sudo apt-get install lzop
+sudo apt-get install git-core curl
+sudo apt-get install u-boot-tools
+sudo apt-get install mtd-utils
+sudo apt-get install android-tools-fsutils
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install device-tree-compiler
+sudo apt-get install gdisk
+
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+
